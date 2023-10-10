efed574ebc841ac41df80071d55fc8bc7d5649f9
Lucas5666 <101807353+Lucas5666@users.noreply.github.com>
2023-10-10 18:27:32 +0800

    L_4

---
+++ Lesson_4_Operation.lua
@@ -0,0 +1,41 @@
+print('**********运算符************')
+print('**********算数运算符************')
+print(1+2)
+a = 1
+b = 2
+print(a + b)
+print('123' + 2)
+print(1 - 2) 
+print('123' * 2)
+print('123' / 2)
+-- 取余
+print('123' % 2)
+-- 幂运算
+print(2^10)
+print('**********条件运算符************')
+print(1 > 2)
+print(1 < 2)
+print(1 == 2)
+-- 不等于
+print(1 ~= 2)
+print(1 >= 2)
+print(1 <= 2)
+print('**********逻辑运算符************')
+-- 支持短路运算
+print(true and true)
+print(true and false)
+print(false or true)
+print(false or false)
+print(not true)
+print(not false and true)
+
+--print(true and print('123'))
+print(false and print('456'))
+
+
+
+
+
+
+
+
