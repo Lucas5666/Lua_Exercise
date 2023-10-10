053e9e510e8b3c7587420eb33c53eade3cfe6eda
Lucas5666 <101807353+Lucas5666@users.noreply.github.com>
2023-10-10 18:54:36 +0800

    L5

---
+++ Lesson_5_Condition.lua
@@ -0,0 +1,23 @@
+print('**********条件语句************')
+a = 9
+if a < 3 then
+    print('a大于3')
+end
+
+if a < 5 then
+    print('a 小于 5')
+else
+    print('a 不小于 5')
+end  
+print('************我是分隔线***************')
+if a < 5 then
+    print('a 小于 5')
+elseif a == 8 then
+    print('a 等于 8')
+elseif a == 9 then
+    print('a等于 9 ')
+end 
+print('************我是分隔线***************')
+if a > 5 and a < 10 then
+    print('a 大于5 小于9')
+end
