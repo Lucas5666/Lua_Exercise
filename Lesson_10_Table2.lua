0c4af03c3e9b98f83c3629fc256de64ebd6daf94
Lucas5666 <101807353+Lucas5666@users.noreply.github.com>
2023-10-11 14:25:17 +0800

    L10

--- Lesson_10_Table2.lua
+++ Lesson_10_Table2.lua
@@ -22,4 +22,45 @@ print(j)
 --print(a[i])
 end
 
+print('************类和结构体***************')
+-- 使用表来模拟类和结构体
+Student = {
+    -- 成员变量
+    age = 18,
+    sex = true,
+    -- 成员函数
+    Up = function()
+        -- age需要指定
+        print(Student.age)
+        print('我长大了')
+    end,
+    Study = function()
+        print('我学习了')  
+    end,
+    Learn = function(p)
+        print(p.sex)
+    end
+}
+-- 把自己当成参数传入函数
+Student.Learn(Student)
+Student:Learn()
+print('************Line***************')
+
+print(Student.age)
+Student.Study()
+Student.name = 'Cai'
+print(Student.name)
+
+Student.Speak = function()
+    print('我说话了')
+end
+Student.Speak()
+
+function Student.Speak2()
+    print('我说话了2')
+end
+Student.Speak2()
+
+Student.Up()
+
 
